7c4d17f5dff0f750bf265cd1930ce97bab144926
MarieG <mariegoudy@gmail.com>
2020-08-12 18:10:50 -0400

    first commit

---
+++ card-game/src/Button.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './App.css';
+import cardBack from './blue_back.jpg';
+import { userCards, compCards} from './index';
+
+export const Button = (props) => {
+    return (<div className="buttoncontainer">
+        <button id="clickme" onClick={props.onClick}>Next Card</button>
+        <button id="clickme" onClick={props.onClick2}>Shuffle Both Hands</button>
+        </div>
+        )
+}
+export const CardDisplay = (props) => {
+    
+    return (
+        <div>
+            <header><h1>Play War</h1></header>
+            <div className="about">
+              <h4 id="user-card-about">User cards remaining: 26</h4>
+              <h4 id="comp-card-about">Computer cards remaining: 26</h4>
+            </div>
+            <div className="card-images">
+              <img src={cardBack} className="card" id="user-card" alt=""/>
+              <img src={cardBack} className="card" id="comp-card" alt="" />
+            </div>
+            <div id={props.isWar ? "war-images" : "invisible"}>
+              <div className="userwar">
+                <img src={cardBack} className="warcard" alt="" />
+                <img src={cardBack} className="warcard" alt="" />
+                <img src={cardBack} className="warcard" alt="" />
+              </div>
+              <div className="compwar">
+                <img src={cardBack} className="warcard" alt="" />
+                <img src={cardBack} className="warcard" alt="" />
+                <img src={cardBack} className="warcard" alt="" /> 
+              </div>
+            </div>
+            <div id={props.isWar ? "showWar" : "invisible" }>
+                <img src={require(`${userCards[3].img}`)} className="warcard1" id={props.isWar ? "usercardwar4" : "invisible" } alt="" />
+                <img src={require(`${compCards[3].img}`)} className="warcard1" id={props.isWar ? "compcardwar4" : "invisible" } alt="" />
+            </div>
+        </div>
+        )
+}
+
---
+++ card-game/src/index.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button, CardDisplay } from './Button';
+import * as serviceWorker from './serviceWorker';
+import './App.css';
+import { cards, shuffle } from './Cards'
+
+
+shuffle(cards);
+
+export const userCards = []
+for (let i = 0; i < 26; i++) {
+    userCards.push(cards[i]);
+}
+export const compCards = []
+for (let i = 26; i < cards.length; i++){
+    compCards.push(cards[i]);
+}
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isWar: false
+    }
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClick2 = this.handleClick2.bind(this);
+
+  }
+  isWar () {
+    return this.state.isWar;
+  }
+  handleClick () {
+    document.getElementById("user-card").src=require(`${userCards[0].img}`);
+    document.getElementById("comp-card").src=require(`${compCards[0].img}`);
+    if (userCards[0].value > compCards[0].value) {
+      userCards.push(userCards[0], compCards[0]);
+      userCards.shift();
+      compCards.shift();
+      this.setState({
+        isWar: false
+      })
+    } else if (userCards[0].value < compCards[0].value) {
+      compCards.push(userCards[0], compCards[0]);
+      userCards.shift();
+      compCards.shift();
+      this.setState({
+        isWar: false
+      })
+    } else if (userCards[0].value === compCards[0].value) {
+      this.setState({
+        isWar: true
+      })
+      if (userCards[3].value >= compCards[3].value) {
+        userCards.push(userCards[0], userCards[1], userCards[2], userCards[3], compCards[0], compCards[1], compCards[2], compCards[3])
+        userCards.splice(0, 4);
+        compCards.splice(0, 4); 
+      } else if (userCards[3].value < compCards[3].value) {
+        compCards.push(userCards[0], userCards[1], userCards[2], userCards[3], compCards[0], compCards[1], compCards[2], compCards[3])
+        userCards.splice(0, 4);
+        compCards.splice(0, 4);
+      }
+
+    }
+    
+    document.getElementById('user-card-about').innerHTML = `User cards remaining: ${userCards.length} `;
+    document.getElementById('comp-card-about').innerHTML = `Computer cards remaining: ${compCards.length}`;
+
+    if(userCards.length === 0) {
+      document.getElementById('comp-card-about').innerHTML = `Computer wins!`;
+    } else if (compCards.length === 0) {
+      document.getElementById('user-card-about').innerHTML = `User wins!`;
+    }
+  };
+
+  handleClick2 () {
+    shuffle(userCards);
+    shuffle(compCards);
+  }
+
+  render() {
+    return (
+    <div>
+      <CardDisplay isWar={this.isWar()} />
+      <Button onClick={this.handleClick} onClick2={this.handleClick2} />
+    </div>)
+  }
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
